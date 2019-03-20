ab2177772ada8ef902312ba837e06b0f8eb52293
tdmiller1 <tdmiller7@bsu.edu>
2019-03-20 19:40:27 -0400

    Add datepicker and delete functionality

--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
 import React, { Component } from 'react';
 import Game from './Dashboard/Game';
+import axios from 'axios';
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+
 
 class App extends Component {
 
   constructor(props){
     super(props)
-        this.state = {
-          games:[],
-          email:"",
-          gameScore:0,
-          date:"",
-          host:""
-        }
+    this.handleDayClick = this.handleDayClick.bind(this);
+    this.state = {
+      games:[],
+      email:"",
+      gameScore:'',
+      date:"",
+      host:"",
+      selectedDay:""
+    };
   }
 
-  componentDidMount(){
+  handleDayClick(day, { selected }) {
+    this.setState({
+      selectedDay: selected ? undefined : day,
+    });
+  }
+
+  componentWillMount(){
     if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
         this.setState({host: "http://localhost:3001"})
     } else {
         this.setState({host: "https://bowling-stats-server.herokuapp.com"})
     }
+    this.setEmail();
+  }
+
+  componentDidMount(){
     this.pullGames();
   }
 
-  checkEmail = _ => {
-    if(localStorage.getItem("email")){
-      const email = JSON.parse(localStorage.getItem("email"));
-      this.setState({email: email})
-      return email
-    }else{
-      return ""
+  setEmail(){
+    const storageEmail = JSON.parse(localStorage.getItem("email"));
+    this.setState({email : storageEmail})
+    this.pullGames();
+  }
+
+  async pullGames(){
+    const allGamesUrl = `${this.state.host}/games/find?id=${this.state.email}`;
+    const response = await axios.get(allGamesUrl)
+    if(response){
+      this.setState({games: response.data.games})
+      console.log(response.data.games)
     }
   }
 
+<<<<<<< HEAD
+  callback(){
+    this.pullGames();
+=======
   pullGames = _ => {
       const email = this.checkEmail();
       var host = ""
@@ -51,37 +76,68 @@ class App extends Component {
           })
         })
       }
+<<<<<<< Updated upstream
+=======
+>>>>>>> 5c2a9f6b072f9dca09d6d70b7185ead6aa3d467a
+>>>>>>> Stashed changes
   }
 
   renderGames = ({_id, score, date}) => 
     <Game key={_id}
+      id={_id}
       score={score}
-      date={date} />
+      date={date}
+      callback={this.callback.bind(this)} />
 
-    addGame = _ => {
-      const url = `${this.state.host}/games/add?id=${this.state.email}&score=${this.state.gameScore}&date=${this.state.date}`;
-        fetch(url)
-        .then(response => response.json())
-        this.pullGames();
+    async addGame(){
+      const addGameUrl = `${this.state.host}/games/add`;
+      const response = await axios.post(addGameUrl,{
+        id:this.state.email,
+        score:this.state.gameScore,
+        date:this.state.selectedDay
+      })
+      if(response){this.pullGames();}
+      
+    }
+
+    handleChange(date) {
+      console.log(date)
     }
 
   render() {
     const { games } = this.state;
+    const STYLE = {backgroundColor:'white'}
+    const inputStyle = {height:'25px'}
     return (
       <div className="app-container">
         <div className="app-sidepanel">
           <div>
-            <input type="text" placeholder="Score" onChange={ (e) => this.setState({gameScore : e.target.value})} />
-            <input type="text" placeholder="Date" onChange={ (e) => this.setState({date : e.target.value})} />
-            <button onClick={() => {
-              this.addGame()
+            <input style={inputStyle} maxLength="3" min="0" max="300" value={this.state.gameScore} placeholder="Score" onChange={ (e) => {this.setState({gameScore : e.target.value}); }} />
+            <div style={STYLE}>
+              <DayPicker
+                  required
+                  selectedDays={this.state.selectedDay}
+                  onDayClick={this.handleDayClick}
+                />
+              </div>
+
+            <button type="submit" onClick={() => {
+                if(this.state.selectedDay == ""){
+
+                }else{
+                  this.addGame();
+                  this.setState({gameScore: ""})
+                }
               }}>Add Game</button>
           </div>
+        </div>
+        <div>
         </div>
         <div className="app-content">
         <table>
           <thead>
             <tr>
+              <th></th>
               <th>Score</th>
               <th>Date</th>
             </tr>
--- src/Dashboard/Game.js
+++ src/Dashboard/Game.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
+import moment from 'moment';
 class Game extends Component {
-  
+
+
+  state = {
+    host:""
+  }
+
+  parseDate(){
+    var date = moment(this.props.date).format('DD-MMM-YYYY');
+    return date
+  }
+
+  componentDidMount(){
+      if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+        this.setState({host: "http://localhost:3001"})
+    } else {
+        this.setState({host: "https://bowling-stats-server.herokuapp.com"})
+    }
+  }
+
+  async delete(){
+    var url = this.state.host + '/games'
+    console.log(this.props)
+    const response = await axios.delete(url,{
+        data: {
+            id: this.props.id
+        }
+    })
+    if(response){
+      this.props.callback()
+    }
+  }
   render() {
     
     return (
       <tr>
+        <td>
+          <button onClick={() => this.delete()}>
+              Delete
+          </button></td>
         <td>{this.props.score}</td>
-        <td>{this.props.date}</td>
+        <td>{this.parseDate()}</td>
       </tr>
       )
     }
